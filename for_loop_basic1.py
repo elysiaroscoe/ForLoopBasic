59c7c184231bdbec45d64a18fbda9f0d32d2836c
Roscoe <elysiaroscoe@gmail.com>
2021-10-25 13:40:11 -0700

    final commit

--- for_loop_basic1.py
+++ for_loop_basic1.py
@@ -17,3 +17,14 @@ for int in range (0,500001):
     if int % 2 != 0:
         sum += int
 print(sum)
+
+for fours in range (2018, 0, -4): #making the 4 negative wasn't intuitive to me, I assumed it would run the range
+    print(fours)
+
+
+lowNum = 0
+highNum = 56
+mult = 3
+for y in range(lowNum, highNum + 1):
+    if y % mult == 0:
+        print(y)
